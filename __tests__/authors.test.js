3126f79d9f4fc441b27feef5c0e1a9c6eb073e6f
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-05 10:52:55 -0600

    post test work all test work

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -9,12 +9,12 @@ describe('author routes', () => {
   });
 
 
-  it.skip('/authors should return a list of authors', async() => {
+  it('/authors should return a list of authors', async() => {
     const res = await request(app).get('/authors');
     expect(res.body.length).toEqual(2);
      
   });
-  it.skip('authors/:id should return authors details', async () => {
+  it('authors/:id should return authors details', async () => {
     const res = await request(app).get('/authors/1');
     expect(res.body).toEqual({
       id: '1',
@@ -25,11 +25,11 @@ describe('author routes', () => {
     });
   });
 });
-it.skip('#POST /authors should create a new author', async () => {
+it('#POST /authors should create a new author', async () => {
   const newAuthor = {
-    'name': 'RL Stine',
-    'dob': '1943',
-    'hometown': 'Columbus OH',
+    name: 'RL Stine',
+    dob: '1943',
+    hometown: 'Columbus OH',
   };
   const res = await request(app).post('/authors').send(newAuthor);
   expect(res.body).toEqual({
--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -9,17 +9,18 @@ describe('book routes', () => {
   });
 
 
-  it.skip('/books should return a list of books', async() => {
+  it('/books should return a list of books', async() => {
     const res = await request(app).get('/books');
     expect(res.body.length).toEqual(8);
      
   });
-  it.skip('books/:id should return data from single book', async () => {
+  it('books/:id should return data from single book', async () => {
     const res = await request(app).get('/books/1');
+    
     expect(res.body).toHaveProperty('id', '1');
     expect(res.body).toHaveProperty('title', 'IT');
     expect(res.body).toHaveProperty('released', '2011');
-    expect(res.body.authors[0]).toHaveProperty('1947');
+    expect(res.body.authors[0]).toHaveProperty('dob');
     expect(res.body.authors[0]).toHaveProperty('hometown');
     expect(res.body.authors[0]).toHaveProperty('id');
     expect(res.body.authors[0]).toHaveProperty('name');
